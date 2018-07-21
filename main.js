ab626287160c86b86917462972aea78f3d90d1b9
Mak Nathan <Mak Nathan>
2018-07-21 17:33:17 +0800

    feat:  打开和展示Markdown文件
    实现了简单的功能
    打开多个文件展示多个tab，点击不同tab展示不同内容

---
+++ main.js
@@ -0,0 +1,25 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+
+function createMainWindow() {
+    let win = null;
+    let windowOptions = {
+        width: 1100,
+        height: 800
+    };
+    win = new BrowserWindow(windowOptions);
+    win.loadURL(path.join('file://', __dirname, 'main.html'));
+    win.on('close', () => {
+        win = null;
+    });
+    win.webContents.openDevTools();
+    win.setTitle('Laurel MarkDown Reader');
+    win.setMenu(null);
+
+}
+app.on("ready", createMainWindow);
+app.on("window-all-closed", () => {
+    app.quit();
+});
